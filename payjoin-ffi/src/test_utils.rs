addbc60e99534edd4f35883767e511b4a0480a4f
Valera <50830352+ValeraFinebits@users.noreply.github.com>
2026-02-24 18:46:30 +0200

    Change type

--- payjoin-ffi/src/test_utils.rs
+++ payjoin-ffi/src/test_utils.rs
@@ -163,7 +163,7 @@ impl TestServices {
         })
     }
 
-    pub fn fetch_ohttp_keys(&self) -> Result<crate::OhttpKeys, crate::io::IoError> {
+    pub fn fetch_ohttp_keys(&self) -> Result<crate::OhttpKeys, crate::core::IoError> {
         let runtime = RUNTIME.lock().expect("Lock should not be poisoned");
         runtime.block_on(async {
             self.0.lock().await.fetch_ohttp_keys().await.map_err(Into::into).map(Into::into)
